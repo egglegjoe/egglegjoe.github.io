3a6900d9740a8d823e0c40a60b95c76da24dd985
MitosisMan <evankostov.5@gmail.com>
2023-05-18 15:47:18 -0500

    added more videos

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-const link = "https://external-preview.redd.it/p3y_aKgo42KXVUQzTnfYWd3EV6F_NdBqZw9oL0nytX8.gif?format=mp4&v=enabled&s=ac0b92c355b13de8b433970107433cb1e51e9f57\nredgif\nhttps://preview.redd.it/yfsr3x8m5wra1.gif?width=378&format=mp4&v=enabled&s=9a485d8d70ed435074247d6d00e051b7095ca5d7\nhttps://i.imgur.com/jJ2H2KS.mp4\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://i.imgur.com/paxS0Rb.mp4\nhttps://preview.redd.it/iv8gj9reuzt91.gif?format=mp4&v=enabled&s=3c09c75bb86bf3934674a35dc46bc6ec634e6523\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://out.reddit.com/t3_10rpl9a?url=https%3A%2F%2Fi.imgur.com%2FGywE6u9.png&token=AQAA0UNlZBcjLCz1q3WtH2vSKND-4XAmlP5QukVGX3WRM6gdf1wg&app_name=web2x&user_id=984984781532&web_redirect=true\nredgif\nredgif\nhttps://i.imgur.com/UMAKKAK.mp4\nredgif\nhttps://i.redd.it/kkktzzds4cl91.jpg\nhttps://i.redd.it/j9clvyy98gx91.jpg\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://i.redd.it/g5gw4xy3in371.jpg\nhttps://i.redd.it/8xaer2gcqjy61.jpg\nhttps://i.imgur.com/ddPktU1.mp4\nredgif\nredgif\nhttps://i.redd.it/csu9zkhvpbw61.jpg\nhttps://out.reddit.com/t3_xzd8e3?url=https%3A%2F%2Fi.imgur.com%2FvaWMLnE.jpg&token=AQAALRtkZJvIB6x6jT_1MxO0KtcgkUZyPUqgxgMLuQeX7iXSrCie&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://i.redd.it/9yfdrpt7hkc61.jpg\nhttps://i.redd.it/nf3flih7yta71.jpg\nhttps://i.redd.it/1smajo2tb2471.jpg\nhttps://out.reddit.com/t3_lrmf4w?url=https%3A%2F%2Fi.imgur.com%2FeNNIcVY.jpg&token=AQAAGxtkZDyRYaTiwO6LFfPkxXFXRzjL_XI94Bs2bzaHWyH_TqZh&app_name=web2x&user_id=984984781532&web_redirect=true\nredgif\nhttps://out.reddit.com/t3_jtd06z?url=https%3A%2F%2Fi.imgur.com%2FmmviYrb.jpg&token=AQAA9BRkZBfFXZ0vSI8NFzDmwMKtZCPiNHmWwPHKle1K6FUDkESB&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://out.reddit.com/t3_k2xwwa?url=https%3A%2F%2Fi.imgur.com%2FIeoUwkS.png&token=AQAAORdkZBOuhdChanKX43bL8vwEATq7_3ED_H0yyZ7RIW0e9Mxn&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://i.redd.it/x1ma47bscg371.jpg\nhttps://i.imgur.com/PeOHBy2.png\nhttps://i.redd.it/2yrxtea2abs61.jpg\nhttps://i.redd.it/ej774x62ctd91.jpg\nhttps://i.redd.it/z2bpkltuu3781.png\nredgif\nredgif\nredgif\nhttps://external-preview.redd.it/Vqk-B-78YUHsW2lCn-jPAaiHREaujfCUY5oOs2g0xG0.png?auto=webp&v=enabled&s=984901c1ea8ee45d6724327dddd377ba51c615a4\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://i.redd.it/n6blmf4mcnc91.jpg\nhttps://out.reddit.com/t3_q6r9tp?url=https%3A%2F%2Fi.imgur.com%2FgPGRlCq.jpg&token=AQAAYEVlZFXiIHqMaShwqw-RIA-CPsEQWGk8EzR54tjkr6ABn_Ms&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://out.reddit.com/t3_uyinbd?url=https%3A%2F%2Fi.imgur.com%2FD7b9j6W.png&token=AQAA8xpkZOoVfjzaq2nKxtmMDFIkGigHaXqYiPGWAqSYdsOZ5ioF&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://out.reddit.com/t3_k12mj5?url=https%3A%2F%2Fi.imgur.com%2FyUR943g.jpg&token=AQAApRVkZLPrgGDhT8e7iU7X9dXdPkC8rIbcYX_tl8uZRKpCDTI3&app_name=web2x&user_id=984984781532&web_redirect=truehttps://i.redd.it/9xfc5ngrex351.jpg\nhttps://out.reddit.com/t3_llx036?url=https%3A%2F%2Fi.imgur.com%2F2Hrtdb6.jpg&token=AQAA2RpkZBXXeZzTESGVhDsOBvbIRLgVcJwBYDyq2ZE0lKmC3ARi&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://out.reddit.com/t3_uhnyqq?url=https%3A%2F%2Fi.imgur.com%2F3QGpOmi.png&token=AQAAtRpkZA2484P1a3AfkBbwbOVkTBblsJLqy7Y0RP1B9p1Qc-wR&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://i.redd.it/9xfc5ngrex351.jpg\nhttps://out.reddit.com/t3_pw6lmo?url=https%3A%2F%2Fi.imgur.com%2FI3qyauv.png&token=AQAAlxpkZJTRJxEp4YwXeInq9-FgAmn5f0erXEpeFNRlOaOF9RdP&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://i.imgur.com/O2iTvgT.mp4\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://i.imgur.com/Rjc6B4C.mp4\nhttps://i.imgur.com/6jRlbH3.mp4\nredgif\nhttps://i.imgur.com/4PtZJBs.mp4\nhttps://i.imgur.com/w5brJ5X.mp4\nredgif\nredgif\nhttps://i.redd.it/95qyjib15kt91.jpg\nhttps://i.imgur.com/zi0Tr76.mp4\nhttps://external-preview.redd.it/NM9OQImpRdEz6dHHuCYYWFr1ZQxBHdTJw0XeMXX2G48.jpg?width=640&crop=smart&auto=webp&v=enabled&s=2798fadc6c16c130c26dc4a03b49957cfe9102a9\nhttps://i.redd.it/bxj6iwtttxb91.jpg\nhttps://preview.redd.it/q5u6ju7ah6j91.jpg?width=1125&format=pjpg&auto=webp&v=enabled&s=ad56115b479b311465624afa633148482bc09305\nhttps://preview.redd.it/846urv7ah6j91.jpg?width=1125&format=pjpg&auto=webp&v=enabled&s=32f4b1c2f95c3cd3a9a0d4baf0495bbba648a623\nhttps://i.redd.it/z2bpkltuu3781.png\nredgif\nredgif\nredgif\nhttps://i.imgur.com/2er6C8L.mp4\nhttps://i.redd.it/587qmebjdlz91.jpg\nredgif\nredgif"
+const link = "https://external-preview.redd.it/p3y_aKgo42KXVUQzTnfYWd3EV6F_NdBqZw9oL0nytX8.gif?format=mp4&v=enabled&s=ac0b92c355b13de8b433970107433cb1e51e9f57\nredgif\nhttps://preview.redd.it/yfsr3x8m5wra1.gif?width=378&format=mp4&v=enabled&s=9a485d8d70ed435074247d6d00e051b7095ca5d7\nhttps://i.imgur.com/jJ2H2KS.mp4\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://i.imgur.com/paxS0Rb.mp4\nhttps://preview.redd.it/iv8gj9reuzt91.gif?format=mp4&v=enabled&s=3c09c75bb86bf3934674a35dc46bc6ec634e6523\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://out.reddit.com/t3_10rpl9a?url=https%3A%2F%2Fi.imgur.com%2FGywE6u9.png&token=AQAA0UNlZBcjLCz1q3WtH2vSKND-4XAmlP5QukVGX3WRM6gdf1wg&app_name=web2x&user_id=984984781532&web_redirect=true\nredgif\nredgif\nhttps://i.imgur.com/UMAKKAK.mp4\nredgif\nhttps://i.redd.it/kkktzzds4cl91.jpg\nhttps://i.redd.it/j9clvyy98gx91.jpg\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://i.redd.it/g5gw4xy3in371.jpg\nhttps://i.redd.it/8xaer2gcqjy61.jpg\nhttps://i.imgur.com/ddPktU1.mp4\nredgif\nredgif\nhttps://i.redd.it/csu9zkhvpbw61.jpg\nhttps://out.reddit.com/t3_xzd8e3?url=https%3A%2F%2Fi.imgur.com%2FvaWMLnE.jpg&token=AQAALRtkZJvIB6x6jT_1MxO0KtcgkUZyPUqgxgMLuQeX7iXSrCie&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://i.redd.it/9yfdrpt7hkc61.jpg\nhttps://i.redd.it/nf3flih7yta71.jpg\nhttps://i.redd.it/1smajo2tb2471.jpg\nhttps://out.reddit.com/t3_lrmf4w?url=https%3A%2F%2Fi.imgur.com%2FeNNIcVY.jpg&token=AQAAGxtkZDyRYaTiwO6LFfPkxXFXRzjL_XI94Bs2bzaHWyH_TqZh&app_name=web2x&user_id=984984781532&web_redirect=true\nredgif\nhttps://out.reddit.com/t3_jtd06z?url=https%3A%2F%2Fi.imgur.com%2FmmviYrb.jpg&token=AQAA9BRkZBfFXZ0vSI8NFzDmwMKtZCPiNHmWwPHKle1K6FUDkESB&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://out.reddit.com/t3_k2xwwa?url=https%3A%2F%2Fi.imgur.com%2FIeoUwkS.png&token=AQAAORdkZBOuhdChanKX43bL8vwEATq7_3ED_H0yyZ7RIW0e9Mxn&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://i.redd.it/x1ma47bscg371.jpg\nhttps://i.imgur.com/PeOHBy2.png\nhttps://i.redd.it/2yrxtea2abs61.jpg\nhttps://i.redd.it/ej774x62ctd91.jpg\nhttps://i.redd.it/z2bpkltuu3781.png\nredgif\nredgif\nredgif\nhttps://external-preview.redd.it/Vqk-B-78YUHsW2lCn-jPAaiHREaujfCUY5oOs2g0xG0.png?auto=webp&v=enabled&s=984901c1ea8ee45d6724327dddd377ba51c615a4\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://i.redd.it/n6blmf4mcnc91.jpg\nhttps://out.reddit.com/t3_q6r9tp?url=https%3A%2F%2Fi.imgur.com%2FgPGRlCq.jpg&token=AQAAYEVlZFXiIHqMaShwqw-RIA-CPsEQWGk8EzR54tjkr6ABn_Ms&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://out.reddit.com/t3_uyinbd?url=https%3A%2F%2Fi.imgur.com%2FD7b9j6W.png&token=AQAA8xpkZOoVfjzaq2nKxtmMDFIkGigHaXqYiPGWAqSYdsOZ5ioF&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://out.reddit.com/t3_k12mj5?url=https%3A%2F%2Fi.imgur.com%2FyUR943g.jpg&token=AQAApRVkZLPrgGDhT8e7iU7X9dXdPkC8rIbcYX_tl8uZRKpCDTI3&app_name=web2x&user_id=984984781532&web_redirect=truehttps://i.redd.it/9xfc5ngrex351.jpg\nhttps://out.reddit.com/t3_llx036?url=https%3A%2F%2Fi.imgur.com%2F2Hrtdb6.jpg&token=AQAA2RpkZBXXeZzTESGVhDsOBvbIRLgVcJwBYDyq2ZE0lKmC3ARi&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://out.reddit.com/t3_uhnyqq?url=https%3A%2F%2Fi.imgur.com%2F3QGpOmi.png&token=AQAAtRpkZA2484P1a3AfkBbwbOVkTBblsJLqy7Y0RP1B9p1Qc-wR&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://i.redd.it/9xfc5ngrex351.jpg\nhttps://out.reddit.com/t3_pw6lmo?url=https%3A%2F%2Fi.imgur.com%2FI3qyauv.png&token=AQAAlxpkZJTRJxEp4YwXeInq9-FgAmn5f0erXEpeFNRlOaOF9RdP&app_name=web2x&user_id=984984781532&web_redirect=true\nhttps://i.imgur.com/O2iTvgT.mp4\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://i.imgur.com/Rjc6B4C.mp4\nhttps://i.imgur.com/6jRlbH3.mp4\nredgif\nhttps://i.imgur.com/4PtZJBs.mp4\nhttps://i.imgur.com/w5brJ5X.mp4\nredgif\nredgif\nhttps://i.redd.it/95qyjib15kt91.jpg\nhttps://i.imgur.com/zi0Tr76.mp4\nhttps://external-preview.redd.it/NM9OQImpRdEz6dHHuCYYWFr1ZQxBHdTJw0XeMXX2G48.jpg?width=640&crop=smart&auto=webp&v=enabled&s=2798fadc6c16c130c26dc4a03b49957cfe9102a9\nhttps://i.redd.it/bxj6iwtttxb91.jpg\nhttps://preview.redd.it/q5u6ju7ah6j91.jpg?width=1125&format=pjpg&auto=webp&v=enabled&s=ad56115b479b311465624afa633148482bc09305\nhttps://preview.redd.it/846urv7ah6j91.jpg?width=1125&format=pjpg&auto=webp&v=enabled&s=32f4b1c2f95c3cd3a9a0d4baf0495bbba648a623\nhttps://i.redd.it/z2bpkltuu3781.png\nredgif\nredgif\nredgif\nhttps://i.imgur.com/2er6C8L.mp4\nhttps://i.redd.it/587qmebjdlz91.jpg\nredgif\nredgif\nhttps://i.imgur.com/u5UxK5X.mp4\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://i.redd.it/4sz2jvq0fbo51.jpg\nhttps://i.imgur.com/Pbk0PQ4.mp4\nhttps://thumbs.gfycat.com/KlutzyJadedIndianrockpython-mobile.mp4\nhttps://out.reddit.com/t3_xs5ufb?app_name=web2x&token=AQAAWkdlZJZPUVBgJf2eAX7ujhaTA5IdK7CK-5rsGF9CaJuhyQ6g&url=https%3A%2F%2Fi.imgur.com%2F5lA1fdk.jpg&user_id=984984781532&web_redirect=true\nhttps://i.redd.it/axykargbyik91.jpg\nhttps://i.redd.it/ncj3oc4xyie91.jpg\nhttps://i.redd.it/qgee8pnspbk91.jpg\nhttps://preview.redd.it/7nufq3e6kgj81.gif?width=400&format=mp4&v=enabled&s=ea858ecbd63335ebdf15e26a4fefbd02ba78a9d2\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nredgif\nhttps://i.redd.it/nulk2hmebls91.jpg\nredgifs"
 var links = link.split('\n')
 
 random()
 
 function random() {
   var rand = Math.floor(Math.random() * links.length)
+  var video = document.getElementById('vid')
+  var image = document.getElementById('img')
 
   if (checkRedgif(links[rand])) {
     var num = rand + 1
-    document.getElementById('vid').src = 'files////' + num + '.mp4'
-    document.getElementById('img').src = ''
-    document.getElementById('img').style.height = '0%'
-    document.getElementById('img').style.width = '0%'
-    document.getElementById('vid').style.height = '100%'
-    document.getElementById('vid').style.width = '100%'
+    if(num < 26 || num > 37){
+    video.src = 'files////' + num + '.mp4'
+    image.src = ''
+    image.style.height = '0%'
+    image.style.width = '0%'
+    video.style.height = '100%'
+    video.style.width = '100%'
+    } else {
+      image.src = 'files////' + num + '.png'
+      video.src = ''
+      video.style.height = '0%'
+      video.style.width = '0%'
+      image.style.height = '100%'
+      image.style.width = '100%'
+    }
   } else if (checkImage(links[rand])) {
-    document.getElementById('img').src = links[rand]
-    document.getElementById('vid').src = ''
-    document.getElementById('vid').style.height = '0%'
-    document.getElementById('vid').style.width = '0%'
-    document.getElementById('img').style.height = '100%'
-    document.getElementById('img').style.width = '100%'
+    image.src = links[rand]
+    video.src = ''
+    video.style.height = '0%'
+    video.style.width = '0%'
+    image.style.height = '100%'
+    image.style.width = '100%'
   } else {
-    document.getElementById('vid').src = links[rand]
-    document.getElementById('img').src = ''
-    document.getElementById('img').style.height = '0%'
-    document.getElementById('img').style.width = '0%'
-    document.getElementById('vid').style.height = '100%'
-    document.getElementById('vid').style.width = '100%'
+    video.src = links[rand]
+    image.src = ''
+    image.style.height = '0%'
+    image.style.width = '0%'
+    video.style.height = '100%'
+    video.style.width = '100%'
   }
   document.getElementById('num').innerHTML = rand + 1
 }
